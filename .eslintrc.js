3cff8ef82eb28b177e118b7c6e80434a4d75b773
Andrey Voronin <AVor0n@yandex.ru>
2021-11-24 16:33:33 +0300

    fix: linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,32 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'plugin:import/typescript', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
   rules: {
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'no-undef': 'off',
+    'no-unused-vars': 'warn',
+    'no-restricted-syntax': 'off',
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        ts: 'never',
+      },
+    ],
+    'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
+    'prettier/prettier': 'error',
+  },
+  globals: {
+    GameMods: 'readonly',
+    Category: 'readonly',
+    Store: 'readonly',
   },
 };
